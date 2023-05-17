11559e361c75e14a572edba2643b276086cdcd36
chandukeertipati <131877124+chandukeertipati@users.noreply.github.com>
2023-05-17 10:59:21 +0530

    working on admin dashboard.

--- src/app/commercial/commercial.component.ts
+++ src/app/commercial/commercial.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-commercial',
   templateUrl: './commercial.component.html',
-  styleUrls: ['./commercial.component.css']
+  styleUrls: ['./commercial.component.css'],
 })
 export class CommercialComponent {
+  jsonData: any;
 
+  constructor(private http: HttpClient) {}
+
+  showData() {
+    this.http.get('http://localhost:3000/signupusers').subscribe((data) => {
+      this.jsonData = data;
+    });
+  }
 }
