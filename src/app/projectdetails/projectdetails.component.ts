5d4b76736a7a40776afe9bc26e7cb1fc8656be63
chandukeertipati <131877124+chandukeertipati@users.noreply.github.com>
2023-05-24 16:47:31 +0530

    created service to get data in my project details.

--- src/app/projectdetails/projectdetails.component.ts
+++ src/app/projectdetails/projectdetails.component.ts
@@ -2,63 +2,40 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { SpaceiService } from '../spacei.service';
 
-// interface Project {
-//   id: number;
-//   name: string;
-//   description: string;
-//   duration: string;
-// }
-// interface Research {
-//   name: string;
-//   email: string;
-//   description: string;
-//   frontendTechnologies: string[];
-//   backendTechnologies: string[];
-// }
-
 @Component({
   selector: 'app-projectdetails',
   templateUrl: './projectdetails.component.html',
   styleUrls: ['./projectdetails.component.css'],
 })
 export class ProjectdetailsComponent {
+  data: any;
+  projects: any[] = [];
   constructor(
     private http: HttpClient,
     private projectdetails: SpaceiService
   ) {}
-  ngOnInit() {}
-    
+  ngOnInit(): void {}
+
   handleClick() {
-    this.projectdetails.getProjectData().subscribe(
-      (data) => {
-        // Handle the retrieved data here
-        console.log(data);
-      },
-      (error) => {
-        // Handle the error accordingly
-        console.error(error);
-      }
-    );
-    }
+    this.projectdetails.getData().subscribe((data: any) => {
+      this.data = data;
+      console.log(this.data);
+    });
   }
 
+  //   this.projectdetails.getProjectData().subscribe(
+  //     (data) => {
+  //       // Handle the retrieved data here
+  //       console.log(data);
+  //     },
+  //     (error) => {
+  //       // Handle the error accordingly
+  //       console.error(error);
+  //     }
+  //   );
+  // }
+}
 
-    
-
-
-
-    function handleClick() {
-      throw new Error('Function not implemented.');
-    }
-// function showdata() {
+// function handleClick() {
 //   throw new Error('Function not implemented.');
-// }
-// function showdata() {
-//   throw new Error('Function not implemented.');
-// }
-// selectedProject: Project | null = null;
-
-// showProjectDetails(project: Project) {
-//   this.selectedProject = project;
-//   console.log(project);
 // }
--- src/app/spacei.service.ts
+++ src/app/spacei.service.ts
@@ -1,19 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SpaceiService {
-  getApiData() {
-    throw new Error('Method not implemented.');
-  }
-  constructor(private http: HttpClient) {}
-  getData() {
-    const apiUrl = 'https://api.github.com/repos/chandukeertipati/spAce.I-SPA';
-    return this.http.get(apiUrl);
-  }
-  getProjectData() {
-    return this.http.get('http://localhost:3000/contactus');
+  // getApiData() {
+  //   throw new Error('Method not implemented.');
+  // }
+  // constructor(private http: HttpClient) {}
+  // getData() {
+  //   const apiUrl = 'https://api.github.com/repos/chandukeertipati/spAce.I-SPA';
+  //   return this.http.get(apiUrl);
+  // }
+  // getProjectData() {
+  //   return this.http.get('http://localhost:3000/contactus');
+  // }
+
+  private apiUrl = 'http://localhost:3000/contactus';
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
   }
 }
