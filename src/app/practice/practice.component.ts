4a17de3df0734733d82e5cae94223e11ded4c531
chandukeertipati <131877124+chandukeertipati@users.noreply.github.com>
2023-08-25 17:35:30 +0530

    added new one python path uploader with new Db

--- src/app/practice/practice.component.ts
+++ src/app/practice/practice.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { PracticeService } from '../practice.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-practice',
   templateUrl: './practice.component.html',
   styleUrls: ['./practice.component.css'],
 })
-export class PracticeComponent  {
+export class PracticeComponent {
+  resumeForm: FormGroup;
 
-
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.resumeForm = this.fb.group({
+      inputPath: ['', Validators.required],
+      outputPath: ['', Validators.required],
+      prompt: ['', Validators.required]
+    });
+  }
   // csvContent: string = '';
   // fileName: string = '';
   // fileSize: number = 0;
@@ -64,4 +73,23 @@ export class PracticeComponent  {
   submitFile(): void {
     this.submitted = true;
   }
+
+
+  // for python
+  submitForm() {
+    if (this.resumeForm.valid) {
+      const formData = this.resumeForm.value;
+      this.http.post('http://localhost:3000/python', formData).subscribe(
+        response => {
+          this.resumeForm.reset();
+          console.log('Data posted successfully', response);
+        },
+        error => {
+          console.error('Error posting data', error);
+        }
+      );
+    }
+  }
 }
+
+
